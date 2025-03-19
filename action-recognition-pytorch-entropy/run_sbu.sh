8ff880e9cea22f15378469b0240c1131cd7511d0
diffwoak <cxy13192381118@163.com>
2025-03-19 09:40:07 +0800

    update remote

--- action-recognition-pytorch-entropy/run_sbu.sh
+++ action-recognition-pytorch-entropy/run_sbu.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 分布式训练环境(默认SBU数据集的配置)
-python train.py --multiprocessing-distributed --datadir /root/autodl-tmp/SBU_splits --dense_sampling --without_t_stride
+python train.py --multiprocessing-distributed --datadir /root/autodl-tmp/SBU_splits --dense_sampling --without_t_stride --lr 0.006 --weight 5
 # 固定BDQ 训练动作识别网络
-python train_target.py --multiprocessing-distributed --datadir /root/autodl-tmp/SBU_splits --dense_sampling --without_t_stride
+python train_target.py --multiprocessing-distributed --datadir /root/autodl-tmp/SBU_splits --dense_sampling --without_t_stride --lr 0.006 --weight 5
 # 固定BDQ 训练隐私识别网络
-python train_budget.py --multiprocessing-distributed --datadir /root/autodl-tmp/SBU_splits --dense_sampling --without_t_stride
+python train_budget.py --multiprocessing-distributed --datadir /root/autodl-tmp/SBU_splits --dense_sampling --without_t_stride --lr 0.006 --weight 5
 ## 测试动作识别
 python test.py --datadir /root/autodl-tmp/SBU_splits --dense_sampling --model_type target  --gpu 0 -b 1 --without_t_stride
 ## 测试隐私识别
